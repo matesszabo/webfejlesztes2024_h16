9ed5401c738b7d41089f389dd4455412c5fc4df8
matesszabo <matesszabo@hotmail.com>
2024-10-07 17:18:59 +0200

    project init

---
+++ src/main/java/hu/unideb/inf/esemenykezelo/EsemenykezeloApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.esemenykezelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsemenykezeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsemenykezeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/esemenykezelo/EsemenykezeloApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.esemenykezelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsemenykezeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
