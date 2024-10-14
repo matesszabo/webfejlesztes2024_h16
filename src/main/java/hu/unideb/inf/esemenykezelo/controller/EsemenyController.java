1d07f1641e6119e7ca9c841b0aa269f7cc0f3d62
matesszabo <matesszabo@hotmail.com>
2024-10-14 16:24:15 +0200

    Hello world

---
+++ src/main/java/hu/unideb/inf/esemenykezelo/controller/EsemenyController.java
@@ -0,0 +1,23 @@
+package hu.unideb.inf.esemenykezelo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//json-t küld, json-t fogad
+@RequestMapping("/api")
+public class EsemenyController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hw")
+    public ResponseEntity<String> helloworld(){
+        return new ResponseEntity<>("Hello, world!", HttpStatus.OK);
+    }
+
+}
