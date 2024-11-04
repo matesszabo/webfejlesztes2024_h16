3679d01d88ef90b0f47de55901e92515e05f475c
matesszabo <matesszabo@hotmail.com>
2024-11-04 17:15:12 +0100

    Mapstruct in FelhasznaloAlapAdatService

--- src/main/java/hu/unideb/inf/esemenykezelo/service/impl/FelhasznaloAlapAdatServiceImpl.java
+++ src/main/java/hu/unideb/inf/esemenykezelo/service/impl/FelhasznaloAlapAdatServiceImpl.java
@@ -1,25 +1,41 @@
 package hu.unideb.inf.esemenykezelo.service.impl;
 
+import hu.unideb.inf.esemenykezelo.data.entity.FelhasznaloEntity;
+import hu.unideb.inf.esemenykezelo.data.repository.FelhasznaloRepository;
 import hu.unideb.inf.esemenykezelo.service.FelhasznaloAlapAdatService;
 import hu.unideb.inf.esemenykezelo.service.dto.FelhasznaloAlapAdatDto;
+import hu.unideb.inf.esemenykezelo.service.mapper.FelhasznaloAlapAdatMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class FelhasznaloAlapAdatServiceImpl implements FelhasznaloAlapAdatService {
+
+    @Autowired
+    FelhasznaloAlapAdatMapper mapper;
+
+    @Autowired
+    FelhasznaloRepository repo;
+
     @Override
     public FelhasznaloAlapAdatDto findById(Long id) {
-        return null;
+        FelhasznaloEntity entity = repo.getReferenceById(id);
+        FelhasznaloAlapAdatDto dto = mapper.felhasznaloEntityToAdatDto(entity);
+        return dto;
     }
 
     @Override
     public List<FelhasznaloAlapAdatDto> findAll() {
-        return List.of();
+        List<FelhasznaloEntity> entities = repo.findAll();
+
+        return mapper.felhasznaloAlapAdatList(entities);
     }
 
     @Override
     public void deleteById(Long id) {
+        repo.deleteById(id);
 
     }
 }
